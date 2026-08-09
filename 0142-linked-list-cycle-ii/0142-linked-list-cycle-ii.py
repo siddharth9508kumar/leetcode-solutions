43951a9c2516083d1494af5ea0244b257f223417
Siddharth Kumar <siddharth9508.kumar@gmail.com>
2026-08-09 21:11:49 +0530

    Time: 0 ms (100%), Space: 22.2 MB (75.4%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -9,22 +9,15 @@ class Solution:
   def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
     slow = head
     fast = head
-
-    # Step 1: Detect if a cycle exists
     while fast and fast.next:
       slow = slow.next
       fast = fast.next.next
-
-      # Cycle detected
       if slow == fast:
-
-        # Step 2: Find the entry node of the cycle
         slow = head
         while slow != fast:
           slow = slow.next
           fast = fast.next
         return slow
-
     return None
 
 
