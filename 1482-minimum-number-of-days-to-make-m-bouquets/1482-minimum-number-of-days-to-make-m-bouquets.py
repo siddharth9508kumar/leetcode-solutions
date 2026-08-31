3ee9a5351adad9fe647d49762b33058c744735cb
Siddharth Kumar <siddharth9508.kumar@gmail.com>
2026-08-31 22:14:18 +0530

    Time: 297 ms (76.09%), Space: 31.7 MB (75.54%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        n = len(bloomDay)
+        # Impossible to make m bouquets if total required flowers exceed array size
+        if m * k > n:
+            return -1
+        
+        left, right = min(bloomDay), max(bloomDay)
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            # Count how many bouquets of size k can be made by day 'mid'
+            bouquets = 0
+            flowers = 0
+            for b in bloomDay:
+                if b <= mid:
+                    flowers += 1
+                    if flowers == k:
+                        bouquets += 1
+                        flowers = 0
+                else:
+                    flowers = 0
+            
+            # Binary search range adjustment
+            if bouquets >= m:
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
