c338777d09cceb8cf39fcb698a7103686a88c3e7
Siddharth Kumar <siddharth9508.kumar@gmail.com>
2026-09-17 21:34:36 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        rob1, rob2 = 0, 0
+        
+        # rob1 = max money stolen up to house i-2
+        # rob2 = max money stolen up to house i-1
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+            
+        return rob2
